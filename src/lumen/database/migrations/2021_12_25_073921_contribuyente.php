1993c4df13465fbae85c38840ac3d4698319a69c
raz <raz@lupercio.mx>
2022-01-25 00:48:28 -0600

    base migrations

---
+++ src/lumen/database/migrations/2021_12_25_073921_contribuyente.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Contribuyente extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    Schema::create('contribuyente', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('rfc');
+            $table->string('email');
+            $table->string('razon_social');
+            $table->timestamps();
+        });        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('contribuyente');
+    }
+}
