e41b366fec09c7b8e5c699aba17d8124de2eb2bf
raz <raz@lupercio.mx>
2022-03-21 02:31:03 -0600

    added facturama API

---
+++ src/lumen/resources/views/descarga.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/descarga.blade.php -->
+
+<x-base>
+
+<h3>descarga factura</h3>
+<a href="<?php echo $pdf?>"><img src="assets/pdf.png" height=50   alt="">PDF</a> 
+<a href="<?php echo $xml?>"><img src="assets/xml.png" height=50   alt="">  XML</a> 
+
+</x-layout>
